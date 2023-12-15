fbefcb90626587f0e1fd360c2d9de8294d9ec361
kabradshaw1 <itllbok82@gmail.com>
2023-12-15 13:51:26 -0500

    section 9 challenge 2

--- stuff.c
+++ stuff.c
@@ -7,14 +7,26 @@
 //   return 0;
 // }
 
+// int main()
+// {
+//   int year;
+//   int age;
+//   printf("Enter current year: ");
+//   scanf("%d", &year);
+//   printf("Enter your current age: ");
+//   scanf("%d", &age);
+//   printf("birth year = %d\n",year-age);
+//   return 0;
+// }
+
 int main()
 {
-  int year;
-  int age;
-  printf("Enter current year: ");
-  scanf("%d", &year);
-  printf("Enter your current age: ");
-  scanf("%d", &age);
-  printf("birth year = %d\n",year-age);
+  double height;
+  double width;
+  printf("Enter height: ");
+  scanf("%d", &height);
+  printf("Enter Width: ");
+  scanf("%d", &width);
+  printf("Area = %d", height/width);
   return 0;
 }
